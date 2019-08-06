38496433efff9672210674ff62e0f5fdbf19d461
Diego Baltieri <diegobaltieri@me.com>
2019-08-06 13:38:59 -0300

    Adjustment

--- src/app/dashboard/control-panel/control-panel.component.ts
+++ src/app/dashboard/control-panel/control-panel.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FilterService} from "../../services/filter.service";
 import Dictionary from "../../services/Dictionary";
+import { NavController } from 'ionic-angular';
+
 
 @Component({
   selector: 'app-control-panel',
@@ -8,9 +10,11 @@ import Dictionary from "../../services/Dictionary";
   styleUrls: ['./control-panel.component.scss']
 })
 export class ControlPanelComponent implements OnInit {
+
+  public rangeObject:any= {lower: 2002, upper: 2015};
   valueSelected: Dictionary[];
 
-  constructor(public fs: FilterService) { }
+  constructor(public fs: FilterService, navCtrl: NavController) { }
 
   ngOnInit() {
     this.fs.currentVar.subscribe(value => this.valueSelected = value);
